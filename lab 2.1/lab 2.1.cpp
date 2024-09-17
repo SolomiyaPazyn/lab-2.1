e034dd724d61b87a1a7a84d0c75bfb9a33689113
Solomiya <solomiia.pazyn.ri.2024@lpnu.ua>
2024-09-17 18:59:22 +0300

    first branch_02 commit

--- lab 2.1/lab 2.1.cpp
+++ lab 2.1/lab 2.1.cpp
@@ -12,18 +12,18 @@ using namespace std;
 int main()
 {
 	double alpha;  // вхідний параметр
-	//double z1; // результат обчислення 1-го виразу
-	double z2;  // результат обчислення 2-го виразу
+	double z1; // результат обчислення 1-го виразу
+	//double z2;  // результат обчислення 2-го виразу
 	// Введення значення альфа
 
 	cout << "alpha ="; cin >> alpha;
 
-	//z1 = 1 - (1.0 / 4) * pow(sin(2 * alpha), 2) + cos(2 * alpha);
-	z2 = pow(cos(alpha), 2) + pow(cos(alpha), 4);
+	z1 = 1 - (1.0 / 4) * pow(sin(2 * alpha), 2) + cos(2 * alpha);
+	//z2 = pow(cos(alpha), 2) + pow(cos(alpha), 4);
 
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
-	cout << "z2 = " << z2 << endl;
+	cout << "z1 = " << z1 << endl;
+	//cout << "z2 = " << z2 << endl;
 
 	cin.get();
 	return 0;
